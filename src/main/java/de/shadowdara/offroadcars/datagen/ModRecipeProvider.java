b76bbbbb66c0c6a16e56b35638a0ec324469632c
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-09-09 19:51:35 +0000

    nnnnn

--- src/main/java/de/shadowdara/offroadcars/datagen/ModRecipeProvider.java
+++ src/main/java/de/shadowdara/offroadcars/datagen/ModRecipeProvider.java
@@ -14,6 +14,22 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter recipeExporter) {
+        ShapedRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_HELMET)
+                .pattern("ASX")
+                .pattern("SBX")
+                .pattern("SXX")
+                .input('A', Items.BLACK_CONCRETE)
+                .input('B', Items.COAL)
+                .input('S', Items.IRON_INGOT)
+                .input('X', Items.IRON_BLOCK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.EMPTY_CANISTER))
+                .offerTo(exporter);
 
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.COMBAT, ModItems.COPPER_HELMET)
+                .pattern("SX")
+                .input('S', Items.EMPTY_CANISTER)
+                .input('X', Items.COAL_BLOCK)
+                .criterion("has_copper_ingot", conditionsFromItem(Items.FUEL_CANISTER))
+                .offerTo(exporter);
     }
 }
